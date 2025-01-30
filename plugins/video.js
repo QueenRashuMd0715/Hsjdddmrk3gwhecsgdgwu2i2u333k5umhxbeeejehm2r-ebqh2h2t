08fb143732493352b1d58615ba4f200515e3ae94
QueenRashuMd0715 <nipunh009@gmail.com>
2025-01-30 14:44:51 +0530

    Update video.js

--- plugins/video.js
+++ plugins/video.js
@@ -49,7 +49,7 @@ cmd({
     if (apiResponse.data.success) {
       const { title, download_url } = apiResponse.data.result;
 
-      await reply(`*╭╼╼╼ ● 𝐐𝐔𝐄𝐄𝐍 𝐑𝐀𝐒𝐇𝐔 𝐌𝐃 ● ╼╼╼╮*\n\n* *QUEEN RASHU MD VIDEO DAWNLODER 🎥*\n\n*╭╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼ ✵*\n*│* Song Name : ${title}\n*│* Duration : ${video.timestemp}\n*╰╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼ ✵*\n\n* *Auto Sending Your Seaching Video👇*\n\n*✵ ╼╼╼╼╼( ʀᴀꜱʜᴜ )╼╼╼╼╼ ✵*\n\n> *𝙿𝙾𝚆𝙴𝙰𝚁𝙳 𝙱𝚈 𝚀𝚄𝙴𝙴𝙽 𝚁𝙰𝚂𝙷𝚄 𝙼𝙳 ❀*`);
+      await reply(`*╭╼╼╼ ● 𝐐𝐔𝐄𝐄𝐍 𝐑𝐀𝐒𝐇𝐔 𝐌𝐃 ● ╼╼╼╮*\n\n* *QUEEN RASHU MD VIDEO DAWNLODER 🎥*\n\n*╭╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼ ✵*\n*│* Song Name : ${title}\n*│* Duration : ${yts.timestamp}\n*╰╼╼╼╼╼╼╼╼╼╼╼╼╼╼╼ ✵*\n\n* *Auto Sending Your Seaching Video👇*\n\n*✵ ╼╼╼╼╼( ʀᴀꜱʜᴜ )╼╼╼╼╼ ✵*\n\n> *𝙿𝙾𝚆𝙴𝙰𝚁𝙳 𝙱𝚈 𝚀𝚄𝙴𝙴𝙽 𝚁𝙰𝚂𝙷𝚄 𝙼𝙳 ❀*`);
 
       await conn.sendMessage(m.chat, { video: { url: download_url }, mimetype: 'video/mp4', caption: `📹 *${title}*\n\n> *𝙿𝙾𝚆𝙴𝙰𝚁𝙳 𝙱𝚈 𝚀𝚄𝙴𝙴𝙽 𝚁𝙰𝚂𝙷𝚄 𝙼𝙳 ❀*` }, { quoted: m });
     } else {
