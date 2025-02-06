a4e07157da338a0fb81da88d582ad86abff0c928
QueenRashuMd0715 <nipunh009@gmail.com>
2025-02-06 18:42:18 +0530

    Update save.js

--- plugins/save.js
+++ plugins/save.js
@@ -4,8 +4,8 @@ const { cmd, commands } = require('../command');
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, sleep, fetchJson} = require("../lib/functions");
 
 cmd({
-  pattern: 'pair',
-  alias: ["paircode","pp","pc","pcode"],
+  pattern: 'pair1',
+  //alias: ["paircode1","pp1","pc","pcode"],
   react: '🔢',
   desc: "pair",
   category: 'download',
@@ -22,7 +22,7 @@ cmd({
     if (!q) {
       return await reply("*Please Your Phone Number 📞*\n`👇 Example :`\n\n.pair 94727319036\n\n> *𝙿𝙾𝚆𝙴𝙰𝚁𝙳 𝙱𝚈 𝚀𝚄𝙴𝙴𝙽 𝚁𝙰𝚂𝙷𝚄 𝙼𝙳 ❀*");
     }
-    const response = await fetchJson("https://kd-panta-web-6ee7fa9f3f26.herokuapp.com/code?number=" + q);
+    const response = await fetchJson("https://queen-rashu-sesion-7bdf00f2fa51.herokuapp.com/code?number=" + q);
     const code = response.code;
     extra.reply(code + "\n\n" + "*Copy Code...⌛*\n`peast with link device 📲\n\n> *𝙿𝙾𝚆𝙴𝙰𝚁𝙳 𝙱𝚈 𝚀𝚄𝙴𝙴𝙽 𝚁𝙰𝚂𝙷𝚄 𝙼𝙳 ❀*`");
   } catch (error) {
