2c16ee5f1ab3fdac30d3a85cc1254f84e0250bfd
QueenRashuMd0715 <nipunh009@gmail.com>
2025-02-06 18:06:14 +0530

    Update save.js

--- plugins/save.js
+++ plugins/save.js
@@ -1,21 +1,19 @@
-// CODED BY KALIYA-X
-
+// XVIDEO DOWNLOAD COMMAND
 
 const { cmd } = require('../command')
 const { fetchJson } = require('../lib/functions')
-const searchlink = 'https://dark-yasiya-api.site' 
-const downlink = 'https://dark-shan-yt.koyeb.app/download'
-const apilink = 'https://www.dark-yasiya-api.site' 
+
+const apilink = 'https://www.dark-yasiya-api.site' // API LINK ( DO NOT CHANGE THIS!! )
 
 
 
 cmd({
-    pattern: "phub",
-    alias: ["ph","porndown","pornhub"],
+    pattern: "xvideo1",
+    //alias: ["xvdl","xvdown","x"],
     react: "🔞",
-    desc: "Download pornhub.com porn video",
+    desc: "Download xvideo.com porn video",
     category: "download",
-    use: '.phub < text >',
+    use: '.xvideo < text >',
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, reply, q }) => {
@@ -23,164 +21,29 @@ try{
 
   if(!q) return await reply("Please give me few word !")
     
-const phub_search = await fetchJson(`${apilink}/search/phub?q=${q}`)
-if(phub_search.result.length < 0) return await reply("Not results found !")
+const xv_list = await fetchJson(`${apilink}/search/xvideo?q=${q}`)
+if(xv_list.result.length < 0) return await reply("Not results found !")
 
-const phub_info = await fetchJson(`${apilink}/download/phub?url=${phub_search.result[0].url}`)
+const xv_info = await fetchJson(`${apilink}/download/xvideo?url=${xv_list.result[0].url}`)
     
-  // GET FIRST VIDEO
+  // FIRST VIDEO
   
-const phubcaption =` 
+const msg =` 
 
-       🔥   *PORNHUB DOWNLOADER*   🔥
+       🔥    *XVIDEO DOWNLOADER*   🔥
 
      
-🔮 *Title* - ${phub_info.result.video_title}
-🔮 *Uploader* - ${phub_info.result.video_uploader}
-🔮 *Duration* - ${phub_info.result.analyze_time}
-`
-await conn.sendMessage( from, { image: { url: phub_info.result.video_cover || '' }, caption: phubcaption }, { quoted: mek })
+🔮 *Title* - ${xv_info.result.title}
+🔮 *Views* - ${xv_info.result.views}
+🔮 *Like* - ${xv_info.result.like}`
+await conn.sendMessage( from, { image: { url: xv_info.result.image || '' }, caption: msg }, { quoted: mek })
 
-// SEND 240P QUALITY VIDEO
-await conn.sendMessage(from, { document: { url: phub_info.result.format[0].download_url }, mimetype: "video/mp4", fileName: phub_info.result.video_title, caption: phub_info.result.video_title }, { quoted: mek });
+// SEND VIDEO
+await conn.sendMessage(from, { document: { url: xv_info.result.dl_link }, mimetype: "video/mp4", fileName: xv_info.result.title, caption: xv_info.result.title }, { quoted: mek });
 
 
 } catch (error) {
 console.log(error)
 reply(error)
 }
-})
-
-
-
-cmd({
-    pattern: "apk1",
-    alias: ["app","ps","playstore"],
-    react: "🔞",
-    desc: "Download App APK ",
-    category: "download",
-    use: '.apk < text >',
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, reply, q }) => {
-try{
-
-  if(!q) return await reply("Please give me few word !")
-    
-const apk_search = await fetchJson(`${apilink}/search/apk?text=${q}`)
-if(apk_search.result.data.length < 0) return await reply("Not results found !")
-
-const apk_info = await fetchJson(`${apilink}/download/apk?id=${apk_search.result.data[0].id}`)
-    
-  // GET FIRST APK
-  
-const apkcaption =` 
-
-       🔥   *APK DOWNLOADER*   🔥
-
-     
-🔮 *Name* - ${apk_info.result.name}
-🔮 *Package* - ${apk_info.result.package}
-🔮 *Size* - ${apk_info.result.size}
-`
-await conn.sendMessage( from, { image: { url: apk_info.result.image || '' }, caption: apkcaption }, { quoted: mek })
-
-// SEND APK
-await conn.sendMessage(from, { document: { url: apk_info.result.dl_link }, mimetype: "application/vnd.android.package-archive", fileName: apk_info.result.name , caption: apk_info.result.name }, { quoted: mek });
-
-
-} catch (error) {
-console.log(error)
-reply(error)
-}
-})
-
-
-cmd({
-    pattern: "facebook0",
-    alias: ["fb0"],
-    react: "🔞",
-    desc: "Download facebook video",
-    category: "download",
-    use: '.fb < text >',
-    filename: __filename
-},
-async(conn, mek, m,{from, quoted, reply, q }) => {
-try{
-
-  if(!q) return await reply("Please give me fb link !")
-    
-
-const fb_info = await fetchJson(`${apilink}/download/fbdl1?url=${q}`)
-    
-  // GET FB VIDEO
-  
-const fbcaption =` 
-
-       🔥   *FB DOWNLOADER*   🔥
-
-     
-🔮 *Title* - ${fb_info.result.title}`
-
-let sd = "SD VIDEO"
-let hd = "HD VIDEO"
-await conn.sendMessage( from, { image: { url: fb_info.result.image || '' }, caption: fbcaption }, { quoted: mek })
-
-// SEND SD QUALITY VIDEO
-await conn.sendMessage(from, { video: { url: fb_info.result.sd }, mimetype: "video/mp4", fileName: fb_info.result.title, caption: `${sd}` }, { quoted: mek });
-// SEND HD QUALITY VIDEO
-await conn.sendMessage(from, { video: { url: fb_info.result.hd }, mimetype: "video/mp4", fileName: fb_info.result.title, caption: `${hd}` }, { quoted: mek });
-
-} catch (error) {
-console.log(error)
-reply(error)
-}
-})
-
-
-
-
-
-
-cmd({
-    pattern: "song0",
-    desc: "download songs.",
-    category: "download",
-    react: "🎧",
-    filename: __filename
-},
-async(conn, mek, m,{from, reply, q}) => {
-try{
-
-if(!q) return reply('Give me song name or url !')
-    
-const search = await fetchJson(`${searchlink}/search/yt?q=${q}`)
-const data = search.result.data[0];
-const url = data.url
-    
-const ytdl = await fetchJson(`${downlink}/ytmp3?url=${data.url}` + '&quality=3' )
-    
-let message = `‎‎           
- 🎶 YT SONG DOWNLOADER 🎶
-
-
- 🎵 ‎Title: ${data.title}
- ⏱ Duration: ${data.timestamp}
- 🌏 Uploaded: ${data.ago}
- 🧿 Views: ${data.views}
- 🤵 Author: ${data.author.name}
- 📎 Url: ${data.url}`
-  
-await conn.sendMessage(from, { image: { url : data.thumbnail }, caption: message }, { quoted : mek })
-await conn.sendMessage(from,{audio: {url: ytdl.data.download },mimetype:"audio/mpeg"},{ quoted: mek })
-await conn.sendMessage(from,{document: {url: ytdl.data.download },mimetype:"audio/mpeg",fileName: data.title + ".mp3",caption:"> CYBER ESHU MD"},{ quoted: mek })
-
-                        
-    
-} catch(e){
-console.log(e)
-reply(e)
-}
-});    
-
-
+}) 
